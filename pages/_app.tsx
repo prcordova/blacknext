386237acbd4176bad223683d8dac52a328cd4ae0
prcordova <pedroricardocordova@yahoo.com.br>
2024-05-22 00:08:22 -0300

    fix:more configs for old version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { CartContextProvider } from "@/hooks/useCart";
+import { CartContextProvider } from "../hooks/useCart";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 
